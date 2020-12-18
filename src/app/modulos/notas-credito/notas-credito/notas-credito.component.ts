cec55fb80fad42d8c00c8048b0377fe102f38af1
gadiel.guerrero <gadiel.guerrero@next-technologies.com.mx>
2020-12-17 23:52:53 -0800

    [add] not a de credito de papel

--- src/app/modulos/notas-credito/notas-credito/notas-credito.component.ts
+++ src/app/modulos/notas-credito/notas-credito/notas-credito.component.ts
@@ -12,6 +12,7 @@ import { IMyDateModel, IMyDpOptions, MyDatePicker } from 'mydatepicker';
 import { CargaDocumentoOC, Usuario } from 'src/app/entidades';
 import { Select2Component } from 'ng2-select2';
 import Swal from 'sweetalert2';
+import { FacturaExtranjeraRCI } from 'src/app/entidades/FacturaExtranjeraRCI';
 declare var $: any;
 @Component({
   selector: 'app-notas-credito',
@@ -25,13 +26,21 @@ export class NotasCreditoComponent implements OnInit, AfterViewInit {
   disabledDataPcickerOptions: IMyDpOptions = {}
 
   public formulario_header: FormGroup;
-  public formulario_documentos: FormGroup;
+  public formulario_papel: FormGroup;
+  public lista_conceptos = new Array();
+
+  public documento_extranjero = new FacturaExtranjeraRCI();
+
 
   public lista_sucursales = new Array();
   public lista_contribuyentes = new Array();
   public identificador_corporativo: string;
   public identificador_contribuyente: string;
   public identificador_nota_credito: string;
+
+  public documento_papel: boolean;
+  public lista_monedas = new Array();
+
 
   public usuario: Usuario;
   public nivel_aprobacion: number = 0;
@@ -74,6 +83,7 @@ export class NotasCreditoComponent implements OnInit, AfterViewInit {
     this.disabledDataPcickerOptions = { ...this.globals.myDatePickerOptions, componentDisabled: true }
     this.fecha_recepcion.updateDateValue(this.fecha_recepcion.getToday());
     this.controlsHeader.correo.setValue(this.usuario.email);
+    this.documento_papel = false;
   }
 
   iniciarFormularios() {
@@ -84,13 +94,24 @@ export class NotasCreditoComponent implements OnInit, AfterViewInit {
       correo: new FormControl({ value: '', disabled: true }, [Validators.required]),
       fecha_recepcion: new FormControl('', Validators.required),
       fecha_pago: new FormControl('', Validators.required),
-    });
-
-    this.formulario_documentos = new FormGroup({
       xml: new FormControl(null, Validators.required),
       pdf: new FormControl(null, Validators.required),
       cuenta_contable: new FormControl('', this.identificador_nota_credito ? Validators.required : null),
       site: new FormControl('', this.identificador_nota_credito ? Validators.required : null),
+    });
+
+    this.formulario_papel = new FormGroup({
+      rfc_emisor: new FormControl('', Validators.required),
+      nombre_emisor: new FormControl('', Validators.required),
+      numero_recibo: new FormControl('', Validators.required),
+      importe: new FormControl('', [Validators.required]),
+      fecha: new FormControl('', Validators.required),
+      impuesto: new FormControl('', Validators.required),
+      moneda: new FormControl('', Validators.required),
+      id_moneda: new FormControl(0, Validators.required),
+      tipo_cambio: new FormControl(''),
+      tasa_cambio: new FormControl(1, Validators.required),
+      // rfc_extranjero: new FormControl('', Validators.required)
     });
   }
 
@@ -101,6 +122,9 @@ export class NotasCreditoComponent implements OnInit, AfterViewInit {
     }, err => {
       console.log(err);
       this.lista_contribuyentes.length = 0;
+    });
+    this.sharedService.obtenerMonedasCorporativo(this.identificador_corporativo).subscribe((data) => {
+      this.lista_monedas = this.globals.agregarSeleccione(this.globals.prepararSelect2(data, 'id', 'nombre'), 'Seleccione moneda...');
     });
   }
 
@@ -135,11 +159,11 @@ export class NotasCreditoComponent implements OnInit, AfterViewInit {
       if (tipo == 'xml') {
         console.log('Es Factura XML,', file.name);
         this.carga_documento.xml = fileData.file_data;
-        this.controlDocumentos.xml.setValue(file.name);
+        this.controlsHeader.xml.setValue(file.name);
       } else {
         console.log('Es Factura PDF,', file.name);
         this.carga_documento.pdf = fileData.file_data;
-        this.controlDocumentos.pdf.setValue(file.name);
+        this.controlsHeader.pdf.setValue(file.name);
       }
 
       // this.amortizacion.pdf = fileData.file_data;
@@ -180,8 +204,9 @@ export class NotasCreditoComponent implements OnInit, AfterViewInit {
   public get controlsHeader(): { [key: string]: AbstractControl } {
     return this.formulario_header.controls;
   }
-  public get controlDocumentos(): { [key: string]: AbstractControl } {
-    return this.formulario_documentos.controls;
+
+  public get formPapelControl(): { [key: string]: AbstractControl } {
+    return this.formulario_papel.controls;
   }
 
 
@@ -202,6 +227,67 @@ export class NotasCreditoComponent implements OnInit, AfterViewInit {
       }
       this.loadingService.hideLoading();
     });
+  }
+
+  resetData() {
+    this.carga_documento = new CargaDocumentoOC();
+    this.lista_conceptos.length = 0;
+    this.formulario_header.reset();
+    this.formulario_header.reset();
+  }
+
+
+  onTipoDocumentoChange(checked: boolean) {
+    this.documento_papel = checked;
+
+    if (this.documento_papel) {
+      this.controlsHeader.xml.clearValidators();
+      this.controlsHeader.xml.updateValueAndValidity()
+    } else {
+      this.controlsHeader.xml.setValidators(Validators.required);
+      this.controlsHeader.xml.updateValueAndValidity()
+    }
+  }
+
+  cargarDocumentoExtranjero() {
+    this.loadingService.showLoading();
+    try {
+      this.carga_documento.identificador_proveedor = this.controlsHeader.unidad_responsable.value;
+      this.carga_documento.identificador_sucursal = this.controlsHeader.sucursal.value;
+      this.documento_extranjero.numero_orden = '';
+      this.documento_extranjero.codigos_recepcion = this.carga_documento.codigos_recepcion;
+      this.documento_extranjero.fecha_comprobante = this.formPapelControl.fecha.value;
+      this.documento_extranjero.moneda = this.formPapelControl.moneda.value;
+      this.documento_extranjero.id_moneda = Number(this.formPapelControl.id_moneda.value);
+      this.documento_extranjero.tipo_cambio = this.formPapelControl.tasa_cambio.value;
+      this.documento_extranjero.numero_comprobante = this.formPapelControl.numero_recibo.value;
+      this.documento_extranjero.total = this.formPapelControl.importe.value;
+      this.documento_extranjero.rfc_proveedor = this.formPapelControl.rfc_emisor.value;
+      this.documento_extranjero.nombre_proveedor = this.formPapelControl.nombre_emisor.value;
+      // this.documento_extranjero.forma_pago = this.documento.orden_compra.forma_pago;
+      this.documento_extranjero.identificador_contribuyente = this.identificador_contribuyente;
+      this.documento_extranjero.sucursal_identificador = this.carga_documento.identificador_sucursal;
+      this.documento_extranjero.identificador_usuario = this.usuario.identificador_usuario;
+      this.documento_extranjero.identificador_corporativo = this.carga_documento.identificador_corporativo;
+      this.documento_extranjero.identificador_proveedor = this.carga_documento.identificador_proveedor
+      this.documento_extranjero.file = this.carga_documento.pdf;
+      this.documento_extranjero.conceptos = this.lista_conceptos;
+
+      console.log(this.documento_extranjero);
+      this.cargaDocumentosService.cargaFacturaProveedorExtRCI(this.documento_extranjero).subscribe((data: any) => {
+        this.loadingService.hideLoading();
+        Swal.fire('Exito', 'Factura cargada correnctamente. Se ha enviado a flujo de aprobación.', 'success');
+        this.loadingService.hideLoading();
+      }, err => {
+        console.log(err);
+        const mensaje = err.error ? err.error.mensaje ? err.error.mensaje : 'Error interno.' : 'Error interno.'
+        Swal.fire('Error', err.error.mensaje ? mensaje : 'Error interno.', 'error')
+        this.loadingService.hideLoading();
+      })
+    } catch (error) {
+      console.log(error);
+      this.loadingService.hideLoading();
+    }
   }
 
 }
