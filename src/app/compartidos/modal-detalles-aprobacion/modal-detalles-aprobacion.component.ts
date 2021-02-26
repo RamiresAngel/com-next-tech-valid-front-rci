4850507a9376396045b2999f482911213a00fe87
Angel <seyito2005@hotmail.com>
2021-02-26 13:26:14 -0600

    [add] ajuste de modal dealle aprobacion

--- src/app/compartidos/modal-detalles-aprobacion/modal-detalles-aprobacion.component.ts
+++ src/app/compartidos/modal-detalles-aprobacion/modal-detalles-aprobacion.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 declare var $: any;
 @Component({
   selector: 'app-modal-detalles-aprobacion',
   templateUrl: './modal-detalles-aprobacion.component.html',
   styles: []
 })
-export class ModalDetallesAprobacionComponent implements OnInit {
-  @Input() detalleAprobacion: any;
-
+export class ModalDetallesAprobacionComponent implements OnInit, OnChanges {
+  @Input() detalleAprobacion: Array<any>;
+  public aux_detalle_aprobacion: Array<any>;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(detalleAprobacion) {
+    this.aux_detalle_aprobacion = detalleAprobacion.detalleAprobacion.currentValue.sort((a: any, b: any) => {
+      return new Date(b.fecha_aprobacion).getTime() - new Date(a.fecha_aprobacion).getTime();
+    })
   }
 
   modalCloseAprobacion() {
