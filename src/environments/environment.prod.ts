161650665a9c2c1c42a8df42aad36455c6fc28ef
gadiel.guerrero <gadiel.guerrero@next-technologies.com.mx>
2021-04-12 18:59:29 -0500

    [add] ligas productivas en enviroment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  host_corporativo: `https://rci.factorecepcion.com/api/v1/validm/corporativo`,
+  host_administracion: `https://rci.factorecepcion.com/api/v1/validm/administracion`,
+  host_documentos: `https://rci.factorecepcion.com/api/v1/validm/documento`,
+  host_gastos_viaje: 'https://rci.factorecepcion.com/api/v1/validm/gastos_viaje',
+  host_republica_dominicana: 'https://rci.factorecepcion.com/api/v1/validm/republica_dominicana',
+  hostSso: 'https://sso.next-technologies.com.mx',
+  host: 'https://rci.factorecepcion.com'
 };
