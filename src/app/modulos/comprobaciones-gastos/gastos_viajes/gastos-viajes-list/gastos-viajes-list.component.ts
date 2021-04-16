1ac29141c582673fe80087fc3cee4fe9a65973de
Eduardo Castellanos <eduardo.castellanos@next-technologies.com.mx>
2021-04-16 16:55:07 -0500

    [update] ajuste Exportar Excel

--- src/app/modulos/comprobaciones-gastos/gastos_viajes/gastos-viajes-list/gastos-viajes-list.component.ts
+++ src/app/modulos/comprobaciones-gastos/gastos_viajes/gastos-viajes-list/gastos-viajes-list.component.ts
@@ -145,53 +145,140 @@ export class GastosViajesListComponent implements OnInit, AfterViewInit {
     });
   }
 
-  getReporte() {
-    // this._comprobacionService.reporteComprobaciones(this.filtro).subscribe((data: any) => {
-    //   this.actualizarTabla();
-    //   this.lista_comprobantes = data.data;
-    //   this.dtTrigger.next();
-    //   this.loadingService.hideLoading();
-    // }, (err) => {
-    //   this.actualizarTabla();
-    //   this.lista_comprobantes.length = 0;
-    //   this.dtTrigger.next();
-    //   this.loadingService.hideLoading();
-    // });
-
-    const token = this._storageService.getDatosIniciales().usuario.token;
-    // return new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': token });
-
-    const aux_filtro = {
-      estatus_id: Number(this.filtro.estatus),
-      estatus: this.filtro.activo,
-      fecha_fin: this.filtro.fecha_fin,
-      fecha_inicio: this.filtro.fecha_inicio,
-      folio_comprobacion: Number(this.filtro.folio_comprobacion),
-      identificador_cc: this.filtro.identificador_cc,
-      identificador_contribuyente: this.filtro.identificador_contribuyente,
-      identificador_corporativo: this.filtro.identificador_corporativo,
-      identificador_usuario: this.filtro.identificador_usuario,
-      tipo_gasto: this.filtro.tipo_gasto,
-    };
-    $('#formID').submit();
-    // $.ajax({
-    //   type: "POST",
-    //   dataType: "json",
-    //   headers: {
-    //     'Authorization': token,
-    //     'Content-Type': 'application/json'
-    //   },
-    //   url: "http://localhost/api/v1/validm/gastos_viaje/comprobacion/reporte/list",
-    //   data: JSON.stringify(aux_filtro),
-    //   success: (data) => {
-    //     window.location.href = data.redirect;
-    //     console.log(data);
-    //   }
-
-    // });
-
-
+  base64 = function (s) {
+    return window.btoa(decodeURI(decodeURIComponent(s)))
   }
+
+  format = function (s, c) {
+    return s.replace(/{(\w+)}/g,
+      function (m, p) {
+        return c[p];
+      })
+  }
+
+  getReporte() {
+    this._comprobacionService.reporteComprobaciones(this.filtro).subscribe((data: any) => {
+      let tabla = `<table class="mitable"><thead>
+      <tr>
+          <th>Compa&ntilde;&iacute;a</th>
+          <th>Tipo Gasto</th>
+          <th>Folio de comprobaci&oacute;n</th>
+          <th>Fecha de Envi&oacute;</th>
+          <th>Empleado</th>
+          <th>Centro de costos</th>
+          <th>Monto reembolsar</th>
+          <th>Recuperable</th>
+          <th>Proveedor</th>
+          <th>Concepto</th>
+          <th>Motivo</th>
+          <th>Destino</th>
+          <th>Observaciones</th>
+          <th>Nacional / Extranjero</th>
+          <th>Tipo de Cambio</th>
+          <th>Moneda</th>
+          <th>Sub Total</th>
+          <th>Impuestos</th>
+          <th>Total</th>
+          <th>Total Reembolso</th>
+          <th>Descripci&oacute;n</th>
+          <th>Jefe Inmediato</th>
+          <th>Estatus</th>
+          <th>Cuenta Contable</th>
+      </tr></thead><tbody>`;
+      data.data.forEach(item => {
+        tabla += `<tr>`;
+        tabla += `<td> ${this.omitirAcentos(item.nombre_compania)} </td>`;
+        tabla += `<td> ${this.omitirAcentos(item.tipo_gasto)} </td>`;
+        tabla += `<td> ${this.omitirAcentos(item.folio_comprobacion)} </td>`;
+        tabla += `<td> ${this.omitirAcentos(item.fecha_envio)} </td>`;
+        tabla += `<td> ${this.omitirAcentos(item.empleado)} </td>`;
+        tabla += `<td> ${this.omitirAcentos(item.nombre_cc)} </td>`;
+        tabla += `<td> ${this.omitirAcentos(item.monto_reembolsar)} </td>`;
+        tabla += `<td> ${this.omitirAcentos(item.recuperable)} </td>`;
+        tabla += `<td> ${this.omitirAcentos(item.proveedor)} </td>`;
+        tabla += `<td> ${this.omitirAcentos(item.concepto)} </td>`;
+        tabla += `<td> ${this.omitirAcentos(item.motivo)} </td>`;
+        tabla += `<td> ${this.omitirAcentos(item.destino)} </td>`;
+        tabla += `<td> ${this.omitirAcentos(item.observaciones)} </td>`;
+        tabla += `<td> ${this.omitirAcentos(item.nacional_extranjero)} </td>`;
+        tabla += `<td> ${this.omitirAcentos(item.tipo_cambio)} </td>`;
+        tabla += `<td> ${this.omitirAcentos(item.nombre_moneda)} </td>`;
+        tabla += `<td> ${this.omitirAcentos(item.subtotal)} </td>`;
+        tabla += `<td> ${this.omitirAcentos(item.total_impuestos_traslados)} </td>`;
+        tabla += `<td> ${this.omitirAcentos(item.total_factura)} </td>`;
+        tabla += `<td> ${this.omitirAcentos(item.total_reembolso)} </td>`;
+        tabla += `<td> ${this.omitirAcentos(item.descripcion)} </td>`;
+        tabla += `<td> ${this.omitirAcentos(item.jefe_inmediato)} </td>`;
+        tabla += `<td> ${this.omitirAcentos(item.estatus)} </td>`;
+        tabla += `<td> ${this.omitirAcentos(item.cuenta_contable)} </td>`;
+        tabla += `</tr>`;
+      });
+      tabla += `</tbody></table>`;
+      this.expoortarReporte(tabla);
+    });
+  }
+
+  omitirAcentos(text) {
+    console.log(text);
+    if (text && text !== '') {
+      var acentos = `ÃÀÁÄÂÈÉËÊÌÍÏÎÒÓÖÔÙÚÜÛãàáäâèéëêìíïîòóöôùúüûÑñÇç`;
+      var original = `AAAAAEEEEIIIIOOOOUUUUaaaaaeeeeiiiioooouuuunncc`;
+      for (var i = 0; i < acentos.length; i++) {
+        if (text) {
+          text = text.replace(acentos.charAt(i), original.charAt(i));
+        } else {
+          text = '';
+        }
+
+      }
+    } else {
+      text = "";
+    }
+    return text;
+  }
+
+  expoortarReporte(data) {
+    console.log(data);
+    const template =
+      `<html xmlns:o="urn:schemas-microsoft-com:office:office"
+          xmlns:x="urn:schemas-microsoft-com:office:excel"
+          xmlns="http://www.w3.org/TR/REC-html40">
+            <head>
+                <!--[if gte mso 9]>
+                <xml>
+                      <x:ExcelWorkbook>
+                        <x:ExcelWorksheets>
+                          <x:ExcelWorksheet>
+                              <x:Name>{worksheet}</x:Name>
+                              <x:WorksheetOptions>
+                              <x:DisplayGridlines/>
+                              </x:WorksheetOptions>
+                          </x:ExcelWorksheet>
+                        </x:ExcelWorksheets>
+                      </x:ExcelWorkbook>
+                </xml>
+              <![endif]-->
+                <meta http-equiv="content-type" content="text/plain; charset=UTF-8"/>
+          </head>
+          <body>
+              {table}
+          </body>
+        </html>`;
+
+    let uri = 'data:application/vnd.ms-excel;base64,'
+    let link = document.createElement('a');
+    var ctx = {
+      worksheet: 'Worksheet', table: data
+    }
+    const filename = "Reporte_Comprobacion_Gastos";
+    link.setAttribute('href', uri + this.base64(this.format(template, ctx)));
+    link.setAttribute('download', filename);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    return false;
+  }
+
   editarBorrador(id: string) {
     id = this._storageService.encriptar_ids(String(id));
     this.router.navigate([`home/comprobaciones/gastos_viaje/edit/${id}`]);
