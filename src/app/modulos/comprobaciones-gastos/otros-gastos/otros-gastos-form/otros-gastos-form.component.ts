53cf4716eaff0444499cc2a13b0650476c539b4f
gadiel.guerrero <gadiel.guerrero@next-technologies.com.mx>
2021-05-18 13:59:12 -0500

    [FIX] es aprobacion en formulario carga

--- src/app/modulos/comprobaciones-gastos/otros-gastos/otros-gastos-form/otros-gastos-form.component.ts
+++ src/app/modulos/comprobaciones-gastos/otros-gastos/otros-gastos-form/otros-gastos-form.component.ts
@@ -152,7 +152,7 @@ export class OtrosGastosFormComponent {
 
 
   obtenerComprobacion() {
-    this._comprobacionService.obtenerHeaderBorrador(this.numero_comprobacion, this.usuario.aprobador).subscribe((data: any) => {
+    this._comprobacionService.obtenerHeaderBorrador(this.numero_comprobacion, (this.aprobacion_data && this.aprobacion_data.is_aprobacion) ? 1 : 0).subscribe((data: any) => {
       this.comprobacion_header = data.data;
       this.lista_comprobantes = this.comprobacion_header.comprobaciones;
       this.totales = { total_gastado: this.comprobacion_header.total_gastado, monto_reembolsable: this.comprobacion_header.monto_reembolsar };
