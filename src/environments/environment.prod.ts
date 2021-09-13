27691eeaaf3f906befbc3a2c2246089e1c9c79d2
Eduardo <eduardo.castellanos@next-technologies.com.mx>
2021-09-13 13:57:49 -0500

    [update] Version prodccuion V2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  host_corporativo: `https://rci.factorecepcion.com/api/v1/validm/corporativo`,
+  host_administracion: `https://rci.factorecepcion.com/api/v1/validm/administracion`,
+  host_documentos: `https://rci.factorecepcion.com/api/v1/validm/documento`,
+  host_gastos_viaje: 'https://rci.factorecepcion.com/api/v1/validm/gastos_viaje',
+  host_republica_dominicana: 'https://rci.factorecepcion.com/api/v1/validm/republica_dominicana',
+  hostSso: 'https://sso.next-technologies.com.mx',
+  host: 'https://rci.factorecepcion.com',
+  host_login_rci: 'https://www.rciauthenticate.com/api'
+  // host_login_rci: 'http://pruebas1.mytrialprogram.com/RCIAuthentication/api'
 };
